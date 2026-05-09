71e62a3b404d0e08e723d8073ae38a58f143fd0c
Jerry Toland <approlific@gmail.com>
2026-05-09 14:22:27 -0500

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Jerry Toland on 5/9/26.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
